a4c607ce49c35619f7df38e799edbfd46d7d93e3
oldmalebird <lgnlgnlgnlgn@gmail.com>
2018-08-06 17:24:28 +0800

    提取海关进出口数据（1 null）
    有一个海关地点Tableau无法识别

--- test.py
+++ test.py
@@ -1,19 +1,20 @@
-#提取省进出口数据
+#提取海关进出口数据
 import pandas as pd
 #python D:\Github\customs_vegetable_data\test.py
 
 #读取原始数据
-docAddress = r'D:\Data\信息中心进出口\原始数据\2018\蔬菜水果_省201801-201803.xls'
-df_origin = pd.read_excel(docAddress,sheet_name='Report', header = None, names = ["产品","地区","当期出口金额（万美元）","当期进口金额（万美元）","当期出口数量（吨）","当期进口数量（吨）","一至当月出口金额（万美元）","一至当月进口金额（万美元）","一至当月出口数量（吨）","一至当月进口数量（吨）"], skiprows = 8)
+docAddress = r'D:\Data\信息中心进出口\原始数据\2018\蔬菜水果_关201801-201803.xls'
+df_origin = pd.read_excel(docAddress,sheet_name='Report', header = None, names = ["产品","海关","当期出口金额（万美元）","当期进口金额（万美元）","当期出口数量（吨）","当期进口数量（吨）","一至当月出口金额（万美元）","一至当月进口金额（万美元）","一至当月出口数量（吨）","一至当月进口数量（吨）"], skiprows = 8)
 '''
 print(df_origin.head(10))
 print(df_origin.tail(10))
 print('读取的df：', len(df_origin.index))
 '''
 #新建一个dataframe，存放读取的dataframe
-df = pd.DataFrame(columns = ["产品", "地区","截至时间","时间","当期出口金额（万美元）","当期进口金额（万美元）","当期出口数量（吨）","当期进口数量（吨）","一至当月出口金额（万美元）","一至当月进口金额（万美元）","一至当月出口数量（吨）","一至当月进口数量（吨）"])
+df = pd.DataFrame(columns = ["产品", "海关","海关地点","截至时间","时间","当期出口金额（万美元）","当期进口金额（万美元）","当期出口数量（吨）","当期进口数量（吨）","一至当月出口金额（万美元）","一至当月进口金额（万美元）","一至当月出口数量（吨）","一至当月进口数量（吨）"])
 df['产品'] = df_origin['产品']
-df['地区'] = df_origin['地区']
+df['海关'] = df_origin['海关']
+df['海关地点'] = df_origin['海关']
 df['当期出口金额（万美元）'] = df_origin['当期出口金额（万美元）']
 df['当期进口金额（万美元）'] = df_origin['当期进口金额（万美元）']
 df['当期出口数量（吨）'] = df_origin['当期出口数量（吨）']
@@ -78,7 +79,7 @@ df['时间'] = df['时间'].str.replace('月', '-1')
 df['时间'] = pd.to_datetime(df['时间']).dt.date
 
 #删除无意义行
-df.dropna(subset = ['地区'], inplace = True)
+df.dropna(subset = ['海关'], inplace = True)
 print('删除无意义行后的行数：', len(df.index))
 
 #填补类别信息
@@ -105,17 +106,25 @@ print('删除‘蔬菜种子.’的行数：', len(df_merge.index))
 df_merge.drop_duplicates(keep = 'first', inplace = True)
 print('删除重复项后的行数：', len(df_merge.index))
 
-#将“内蒙”替换成“内蒙古”
-df_merge['地区'] = df_merge['地区'].str.replace('内蒙', '内蒙古')
+#删除海关地点中的“海关”、“关区”字样
+#df_merge['海关地点'] = df_merge['海关地点'][:-2] 这个是删除了海关地点那列的最后两个数据
+df_merge['海关地点'] = df_merge['海关地点'].str.replace('海关', '')
+df_merge['海关地点'] = df_merge['海关地点'].str.replace('关区', '')
+
+#海关地点替换：“拱北”改为“珠海”，“黄埔”改为“广州”，“海口”改为“海口市”，以便数据可视化软件对该地点进行识别
+df_merge['海关地点'] = df_merge['海关地点'].str.replace('拱北', '珠海')
+df_merge['海关地点'] = df_merge['海关地点'].str.replace('海口', '海口市')
+df_merge['海关地点'] = df_merge['海关地点'].str.replace('黄埔', '广州')
+
 
 #添加不含合计的数据
-df_no_sum = df_merge.loc[df_merge['地区'] != '全国合计']
+df_no_sum = df_merge.loc[df_merge['海关地点'] != '关口合计']
 print('不含合计数的行数：', len(df_no_sum.index))
 
 
 writer = pd.ExcelWriter(r"C:\Users\cva_b\Desktop\test.xlsx")
 df_no_sum.to_excel(writer, sheet_name='Cleaned', index = False)
-df_merge.to_excel(writer, sheet_name='Cleaned含全国合计', index = False)
+df_merge.to_excel(writer, sheet_name='Cleaned含关口合计', index = False)
 
 
 #python D:\Github\customs_vegetable_data\test.py
