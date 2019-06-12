a438b7f507e789c85bbcfa926b662cd3a05676d0
oldmalebird <lgnlgnlgnlgn@gmail.com>
2019-06-12 09:44:55 +0800

    Update 遍历文件夹中的文件.py

--- 遍历文件夹中的文件.py
+++ 遍历文件夹中的文件.py
@@ -4,12 +4,12 @@
 '''
 
 import os
-dir = r"D:\Data\新发地菜价\price_raw"
+dir = r"D:\张慕明\2018产业杰出人物\产业杰出人物在2019产业大会的宣传资料\挑选出来的照片"
 dirs = os.listdir(dir)
-'''for dirc in dirs:
+for dirc in dirs:
     print(dirc)
 '''
 print(dirs)
 print(len(dirs))
-
+'''
 #python D:\Github\customs_vegetable_data\遍历文件夹中的文件.py
